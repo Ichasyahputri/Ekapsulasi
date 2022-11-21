da0ab84e63080c8fbb4753214e801bf83aa9cdd1
Ichasyahputri <118749731+Ichasyahputri@users.noreply.github.com>
2022-11-21 22:05:53 +0700

    Add files via upload

---
+++ Ekapsulasi_icha.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ekapsulasi_icha;
+
+/**
+ *
+ * @author Icha Syahputri
+ */
+public class Ekapsulasi_icha {
+
+public static void main(String[] args) {
+                Persegi P = new Persegi();
+        P.setPanjang(7);    
+        P.setLebar(6);
+        System.out.println("Program persegi panjang");
+        System.out.println("Tugas Icha syahputri");
+        System.out.println("Panjang: " +P.getPanjang());
+        System.out.println("Lebar: " +P.getLebar());
+        System.out.print("Luas persegi: " +P.getLuas());
+}}
